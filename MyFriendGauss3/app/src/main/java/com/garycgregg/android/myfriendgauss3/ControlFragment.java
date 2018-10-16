420cdcbd6bd17a39b388ab2976eb98cf00efbea1
Gary Gregg <garygr@uw.edu>
2018-10-16 14:01:47 -0700

    Update the name in the problem in onPause().

--- MyFriendGauss3/app/src/main/java/com/garycgregg/android/myfriendgauss3/ControlFragment.java
+++ MyFriendGauss3/app/src/main/java/com/garycgregg/android/myfriendgauss3/ControlFragment.java
@@ -3,27 +3,39 @@ package com.garycgregg.android.myfriendgauss3;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.EditText;
 
 public class ControlFragment extends CardFragment {
 
     private Problem problem;
+    private EditText problemNameEditText;
 
     public static CardFragment createInstance() {
         return new ControlFragment();
     }
 
     @Override
+    public void onPause() {
+
+        super.onPause();
+        problem.setName(problemNameEditText.getText().toString());
+        getProblemLab().updateName(problem);
+    }
+
+    @Override
     protected void createContent(LayoutInflater inflater, ViewGroup container) {
 
         final View view = inflater.inflate(R.layout.content_control, container, true);
+        problemNameEditText = view.findViewById(R.id.problem_name);
+
         final long problemId = getProblemId();
-
         problem = getProblemLab().getProblem(problemId);
-        if (null != problem) {
+        if (null == problem) {
 
-            final TextView textView = view.findViewById(R.id.problem_name);
-            textView.setText(problem.getName());
+            problem = new Problem();
+            problem.setProblemId(problemId);
         }
+
+        problemNameEditText.setText(problem.getName());
     }
 }
